17aeb5930039cc1d1dc96be265fe2812a08d67e0
Dennis van den Heerik <dennis@mpluskassa.nl>
2020-09-15 17:40:39 +0200

    WIP

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -5,73 +5,61 @@ namespace Mpluskassa\ApiClient;
 use GuzzleHttp\Client;
 use SimpleXMLElement;
 use DOMDocument;
+use DOMElement;
 use DOMAttr;
 
 class ApiClient {
 
     private const VERSION = "1.0.0";
-    private const SOAP_HEADER = '<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"><SOAP-ENV:Body>';
-    private const SOAP_FOOTER = '</SOAP-ENV:Body></SOAP-ENV:Envelope>';
 
     private Client $client;
-    private string $apiUrl;
-    private string $query;
+    private float $duration = 0;
 
     public function __construct(string $apiServer, int $apiPort, string $ident, string $secret) {
-        $this->apiUrl = sprintf("%s:%u", $apiServer, $apiPort);
         $this->client = new Client([
-            'base_uri' => $this->apiUrl,
-//            'auth' => [
-//                $ident,
-//                $secret
-//            ],
+            'base_uri' => sprintf("%s:%u", $apiServer, $apiPort),
             'headers' => [
                 'User-Agent' => sprintf("%s %s", __CLASS__, self::VERSION),
                 'Content-Type' => 'text/xml; charset=utf-8',
                 'SOAPAction' => ''
             ],
-//            'query' => [
-//                'ident' => $ident,
-//                'secret' => $secret
-//            ]
+            'query' => [
+                'ident' => $ident,
+                'secret' => $secret
+            ]
         ]);
     }
 
-    public function execute(string $method, $data = null) {
-
-        //$cleanXml = $dom->ownerDocument->saveXML($dom->ownerDocument->documentElement);
-//        $xml = new SimpleXMLElement(sprintf('<ns1:%s xmlns:ns1="urn:mplusqapi"></ns1:%s>', $method, $method), 0, false, "ns1='urn:mplusqapi'");
-//        $customXML = new SimpleXMLElement($xml->asXML());
-//        $dom = dom_import_simplexml($customXML);
-//        $cleanXml = $dom->ownerDocument->saveXML($dom->ownerDocument->documentElement);
-//        $xmlRequest = sprintf("%s%s%s", self::SOAP_HEADER, $cleanXml, self::SOAP_FOOTER);
-
-        $xml = $this->createXML($method, $data);
-        die(print_r($xml, true));
-        
-        
+    public function execute(string $method, ?object $requestObject = null): SimpleXMLElement {
+        $startTime = microtime(true);
+        $requestXML = $this->createXML($method, $requestObject);
+        die(var_export($requestXML, true));
         try {
-            $response = $this->client->post(
-                    "/",
-                    [
-                        'body' => $xml,
-                    ]
-            );
-
-//            var_dump($response);
-            if ($response->getStatusCode() === 200) {
-                // Success!
-                $xmlResponse = simplexml_load_string($response->getBody()); // Convert response into object for easier parsing
-                echo "Response : " . var_export($xmlResponse, true);
+            $response = $this->client->post("/", ['body' => $requestXML]);
+            if (($responseCode = $response->getStatusCode()) === 200) {
+                $responseXML = $response->getBody()->getContents();
+                echo "Response xml : " . $responseXML;
+                if (($parsedXML = simplexml_load_string($responseXML)) === false) {
+                    throw new \Exception("Could not parse XML");
+                }
+                if (is_array($returnValue = $parsedXML->xpath(sprintf('//ns:%sResponse', ucfirst($method)))) && count($returnValue) && is_object(reset($returnValue))) {
+                    $this->duration = microtime(true) - $startTime;
+                    return reset($returnValue);
+                }
+                throw new \Exception("No valid response");
             } else {
-                echo 'Response Failure !!!';
+                throw new \Exception("Received a HTTP Code : " . $responseCode);
             }
         } catch (\Exception $e) {
-            echo 'Exception:' . $e->getMessage();
+            throw new Exception('Exception:' . $e->getMessage());
         }
     }
 
-    private function createXML($method, $data) {
+    public function getLastCallDurationInSeconds(): float {
+        return round($this->duration, 1);
+    }
+
+    private function createXML(string $method, ?object $requestObject): string {
         $dom = new DOMDocument();
         $dom->encoding = 'utf-8';
         $dom->xmlVersion = '1.0';
@@ -81,13 +69,35 @@ class ApiClient {
         $soapEnv->setAttribute('xmlns:SOAP-ENC', 'http://schemas.xmlsoap.org/soap/encoding/');
         $soapEnv->setAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
         $soapEnv->setAttribute('xmlns:xsd', 'http://www.w3.org/2001/XMLSchema');
-        $soapEnv->setAttribute('xmlns:ns', 'urn:mplusqapi');
-        $soapBody = $dom->createElement("SOAP-ENV:BODY");
+        $soapEnv->setAttribute('xmlns:ns1', 'urn:mplusqapi');
+        $soapBody = $dom->createElement("SOAP-ENV:Body");
         $methodElement = $dom->createElement(sprintf("ns1:%s", $method));
+        $this->addRequestObject($dom, $methodElement, $requestObject);
         $soapBody->appendChild($methodElement);
         $soapEnv->appendChild($soapBody);
         $dom->appendChild($soapEnv);
         return $dom->saveXML();
+    }
+
+    private function addRequestObject(DOMDocument &$dom, DOMElement &$methodElement, $requestObject): void {
+        if ($requestObject !== null) {
+//            die(print_r($requestObject, true));
+            if(is_array($requestObject)) {
+                foreach($requestObject as $key => $requestObjectItem) {
+                    $this->addRequestObject($dom, $methodElement, $requestObject[$key]);
+                }
+            } elseif(is_object($requestObject)) {
+                foreach($requestObject as $key => $value) {
+                    $requestElement = $dom->createElement($key);
+                    $methodElement->appendChild($requestElement);
+                    if(is_array($value) || is_object($value)) {
+                        $this->addRequestObject($dom, $requestElement, $value);
+                    } else {
+                        
+                    }
+                }
+            }
+        }
     }
 
 }
